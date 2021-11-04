cd240464f743ce0d4a33855ef0b64d283b7d45fa
Halo-Michael <yc992147156@me.com>
2021-11-05 03:12:59 +0800

    Update

--- make-application.sh
+++ make-application.sh
@@ -15,6 +15,8 @@ cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchains/ios.cmake ..
 make -j8
 cp ../ext/vulkan/iOS/Frameworks/libMoltenVK.dylib PPSSPP.app/Frameworks
 ln -s ./ Payload
-ldid -S../../ent.xml PPSSPP.app/PPSSPP
+mv PPSSPP.app/PPSSPP PPSSPP.app/org.ppsspp.ppsspp
+ldid -S../../ent.xml PPSSPP.app/org.ppsspp.ppsspp
+mv PPSSPP.app/org.ppsspp.ppsspp PPSSPP.app/PPSSPP
 version_number=`echo "$(git describe --tags --match="v*" | sed -e 's@-\([^-]*\)-\([^-]*\)$@-\1-\2@;s@^v@@;s@%@~@g')"`
 echo ${version_number} > PPSSPP.app/Version.txt
